b1c99ba06595dc54118ecbb0a86c271ffb1e6721
Diana <dtenirbergenova@gmail.com>
2023-05-14 02:30:56 +0600

    docker

--- FYP/settings.py
+++ FYP/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'FYP.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'HOST': 'database',
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'PORT': '5432'
     }
 }
 
